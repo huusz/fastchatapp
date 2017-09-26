89bbbb6ea9e2252c34e7f17d84888725f93910dc
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-09-26 20:14:17 +0900

    Add mockData

--- src/App.js
+++ src/App.js
@@ -7,9 +7,51 @@ import VoteScore from './components/VoteScore';
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.css';
 
+const mockData = {
+  questions: [
+    {
+      firstOption: '자장면',
+      secondOption: '짬뽕',
+      firstOptionImage: 'http://bulma.io/images/placeholders/96x96.png',
+      secondOptionImage: 'http://bulma.io/images/placeholders/96x96.png',
+      posted_by: {
+        name: '소용석',
+        email: 'ysoh611@gmail.com',
+        photoUrl: 'http://bulma.io/images/placeholders/96x96.png',
+      },
+      firstOptionVoteList: [
+        {
+          name: '홍지수',
+        },
+        {
+          name: '홍지수',
+        },
+        {
+          name: '홍',
+        },                
+      ],
+      secondOptionVoteList: [
+        {
+          name: '리액트',
+        },                
+        {
+          name: '마크',
+        },                
+      ]
+    },
+  ]
+}
+
 class App extends Component {
   state = {
     isPostMode: false,
+    questions: [],
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      questions: mockData.questions,
+    })
   }
 
   togglePostingMode = () => this.setState({ isPostMode: !this.state.isPostMode })
